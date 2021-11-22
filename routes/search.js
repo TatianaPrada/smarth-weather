687c9997cb880ad5e19eba3cd72eee89f52748df
TatianaPrada <tatiana.1227@hotmail.com>
2021-11-22 00:39:08 +0000

    routes defined

--- routes/search.js
+++ routes/search.js
@@ -2,22 +2,15 @@ const router = require("express").Router();
 const axios = require("axios")
 //API 
 const net = require('follow-redirects').https;
-const auth_key = Buffer.from(process.env.API_ACCESS).toString('base64');
+const auth_key = Buffer.from(`${process.env.API_KEY}:${process.env.API_PASS}`).toString('base64');
 
 //My own middleware
 const {isLoggedIn} = require("../middleware/route-guard")
-
 
 //GET routes
 router.get("/search", (req, res) => {
   res.render("destinations/search.hbs");
 });
-
-//GET routes
-router.get("/search/results", (req, res) => {
-    res.render("destinations/results.hbs");
-  });
-  
 
 
 // const options = {
@@ -33,16 +26,17 @@ router.get("/search/results", (req, res) => {
 
 
 //POST route 
-router.get("/search", async (req, res) => {
-    const destFromForm = req.query;
+router.get("/search/results", async (req, res) => {
+    const destFromForm = req.query.destinationName;
     console.log(destFromForm)
     try{
         const apiCall = await axios(`https://api.roadgoat.com/api/v2/destinations/auto_complete?q=${destFromForm}`,
         {headers: {'Authorization': `Basic ${auth_key}`}
     })
-    const destinations = apiCall.data
-    console.log(destinations)
-    res.render('search/results', {destinations})
+
+    const destinations = apiCall.data.data
+    console.log(destinations[0].relationships.featured_photo)
+    res.render("destinations/results", {info: destinations, relationships: destinations[0].relationships})
     } catch(err){
         console.log((err))
     }
