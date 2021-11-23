7bfd55de1aa1ef23495612543f12fe96239540a0
TatianaPrada <tatiana.1227@hotmail.com>
2021-11-23 11:46:56 +0000

    changes done

--- routes/search.js
+++ routes/search.js
@@ -12,40 +12,37 @@ router.get("/search", (req, res) => {
   res.render("cities/search.hbs");
 });
 
-
-//GET Rroute for each city
-
-router.get("/results/city/:cityName", async (req, res) => {
-    
-    const cityName = req.query.cityName
-    console.log(cityName)
-    try{
-        const apiCall = await axios(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_KEY}&q=${cityName}&days=1&aqi=no&alerts=no`)
-        const cityDetais = apiCall
-        //console.log(cityDetais)
-        res.render("cities/results", {cityDetais})
-
-    } catch(err){
-        console.log((err))
-    }
-    res.render("cities/city.hbs");
-});
-
-
-//POST route 
 router.get("/search/results", async (req, res) => {
     const destFromForm = req.query.destinationName;
     console.log(destFromForm)
     try{
-         const apiCall = await axios(`http://api.weatherapi.com/v1/search.json?key=${process.env.WEATHER_KEY}&q=${destFromForm}&aqi=yes`)
+        const apiCall = await axios(`http://api.weatherapi.com/v1/search.json?key=${process.env.WEATHER_KEY}&q=${destFromForm}&aqi=yes`)
         const destinations = apiCall.data
-        console.log(destinations)
+        //console.log(destinations)
         res.render("cities/results", {destinations})
 
     } catch(err){
         console.log((err))
     }
 })
+
+//GET Rroute for each city
+
+router.get("/results/city/:cityName", async (req, res) => {
+    
+    const cityName = req.params.cityName
+    console.log(cityName)
+    try{
+        const apiCall = await axios(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_KEY}&q=${cityName}&days=1&aqi=no&alerts=no`)
+        const cityDetais = apiCall.data
+        console.log(cityDetais)
+        res.render("cities/city", {cityDetais})
+
+    } catch(err){
+        console.log((err))
+    }
+    //res.render("cities/results", {cityDetais})
+});
 
 
 
