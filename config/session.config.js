e6d8dea2958d0fcbabcb781b7d8e0921367e55f7
Tatiana Prada <tatiana.1227@hotmail.com>
2022-01-05 23:47:27 +0000

    package json updated

--- config/session.config.js
+++ config/session.config.js
@@ -8,12 +8,13 @@ module.exports = (app) => {
   app.use(
     session({
       secret: process.env.SESS_SECRET,
-      resave: true,
-      saveUninitialized: false,
+      resave: false,
+      saveUninitialized: true,
       cookie: {
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === "production",
         httpOnly: true,
+        maxAge:  60 * 60 * 24,
       },
       store: MongoStore.create({
         mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/basic-auth",
