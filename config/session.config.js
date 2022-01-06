87d755803d20a70edae31f81fc43f6e441117b21
Tatiana Prada <tatiana.1227@hotmail.com>
2022-01-06 11:18:01 +0000

    changes

--- config/session.config.js
+++ config/session.config.js
@@ -5,24 +5,25 @@ const MongoStore = require("connect-mongo");
 const mongoose = require('mongoose');
 
 
-module.exports = app => {
-  app.set('trust proxy', 1);
- 
+module.exports = (app) => {
+  app.set("trust proxy", 1);
+
   app.use(
     session({
       secret: process.env.SESS_SECRET,
       resave: true,
       saveUninitialized: false,
       cookie: {
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        secure: process.env.NODE_ENV === 'production',
-        httpOnly: true
-      }, 
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        secure: process.env.NODE_ENV === "production",
+        httpOnly: true,
+      },
       store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/lab-express-basic-auth',
+        mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/basic-auth",
+
         // ttl => time to live
-        ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
-      })
+        ttl: 60 * 60 * 24, // 60sec * 60min * 24h => 1 day
+      }),
     })
   );
 };
